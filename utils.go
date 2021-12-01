98f9dd8e6fc9ce31f876b5e194e83b8dfcb04fa3
A. Fox <anthonyfox1988@gmail.com>
2021-11-30 23:12:07 -0800

    Add utility method to read integers from file

---
+++ utils.go
@@ -0,0 +1,32 @@
+package main
+
+import (
+	"os"
+	"strconv"
+	"strings"
+)
+
+func ReadIntegersFromFile(filename string) []int {
+	body, err := os.ReadFile(filename)
+	if err != nil {
+		return []int{}
+	}
+
+	rawInputs := strings.Split(string(body), "\n")
+
+	inputs := []int{}
+	for i := 0; i < len(rawInputs); i++ {
+		value := rawInputs[i]
+		if value == "" {
+			continue
+		}
+
+		intValue, err := strconv.Atoi(value)
+		if err != nil {
+			return []int{}
+		}
+		inputs = append(inputs, intValue)
+	}
+
+	return inputs
+}
