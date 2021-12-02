c4927a9b4ec7a2967a6d8b92b3d9922e5868910e
A. Fox <anthonyfox1988@gmail.com>
2021-12-02 13:22:06 -0800

    rename utils to util

--- util/util.go
+++ util/util.go
@@ -1,10 +1,34 @@
-package utils
+package util
 
 import (
 	"os"
 	"strconv"
 	"strings"
 )
+
+func ReadStringsFromFile(filename string) []string {
+	body, err := os.ReadFile(filename)
+	if err != nil {
+		return []string{}
+	}
+
+	rawInputs := strings.Split(string(body), "\n")
+
+	inputs := []string{}
+	for i := 0; i < len(rawInputs); i++ {
+		value := rawInputs[i]
+		if value == "" {
+			continue
+		}
+
+		if err != nil {
+			return []string{}
+		}
+		inputs = append(inputs, value)
+	}
+
+	return inputs
+}
 
 func ReadIntegersFromFile(filename string) []int {
 	body, err := os.ReadFile(filename)
