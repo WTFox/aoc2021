52b49cbf6068e21d0a95b2c1a6259d50ecf917de
A. Fox <anthonyfox1988@gmail.com>
2021-12-07 09:07:30 -0800

    refactorings

--- main.go
+++ main.go
@@ -11,45 +11,23 @@ func main() {
 	Day07()
 }
 
-func Day01() {
-	depths := util.ReadIntegersFromFile("./inputs/day01.txt")
-	fmt.Println(countNumberOfPositiveChangesInDepth(depths))
+func Day07() {
+	inputString := util.ReadStringsFromFile("./inputs/day07-crabs-in-submarines.txt")[0]
+	crabSubmarines := util.StringToIntSlice(inputString)
+
+	fmt.Println(submarine.FindMostEfficientFuelUsage(crabSubmarines))
 }
 
-func Day02() {
-	instructions := util.ReadStringsFromFile("./inputs/day02.txt")
-	s := submarine.New()
-	s.ProcessInstructions(instructions)
-	fmt.Println(s.Result())
+func Day06() {
+	fishesAsString := util.ReadStringsFromFile("./inputs/day06-lanternfish.txt")[0]
+	fishies := util.StringToIntSlice(fishesAsString)
+
+	fmt.Println(submarine.SimulateLanternFishLife(fishies, 256))
 }
 
-func Day03() {
-	bytes := util.ReadStringsFromFile("./inputs/day03.txt")
-	d := submarine.NewDiagnosticReport(bytes)
-	d.Process()
-	fmt.Println(d.LifeSupportRating())
-}
-
-func Day04Part1() {
-	inputs := util.ReadStringsFromFile("./inputs/day04.txt")
-	bingoBoards := submarine.NewBingoMultiple(inputs)
-	fmt.Printf("%d boards\n", len(bingoBoards))
-
-	quickestWinner := &bingoBoards[0]
-	for index := 0; index < len(bingoBoards); index++ {
-		bingo := &bingoBoards[index]
-		bingo.Play()
-		if bingo.HasBingo() {
-			score := bingo.Score()
-			if bingo.NumTurns < quickestWinner.NumTurns {
-				fmt.Printf("Board %d won in %d moves with score %d.\n", index, bingo.NumTurns, score)
-				quickestWinner = bingo
-				continue
-			}
-		}
-	}
-
-	fmt.Printf("Quickest board won in %d moves with score %d.\n", quickestWinner.NumTurns, quickestWinner.Score())
+func Day05() {
+	inputs := util.ReadStringsFromFile("./inputs/day05.txt")
+	fmt.Println(submarine.SumVentOverlaps(inputs))
 }
 
 func Day04Part2() {
@@ -74,25 +52,47 @@ func Day04Part2() {
 	fmt.Printf("Last board won in %d moves with score %d.\n", lastWinner.NumTurns, lastWinner.Score())
 }
 
-func Day05() {
-	inputs := util.ReadStringsFromFile("./inputs/day05.txt")
-	fmt.Println(submarine.SumVentOverlaps(inputs))
+func Day04Part1() {
+	inputs := util.ReadStringsFromFile("./inputs/day04.txt")
+	bingoBoards := submarine.NewBingoMultiple(inputs)
+	fmt.Printf("%d boards\n", len(bingoBoards))
+
+	quickestWinner := &bingoBoards[0]
+	for index := 0; index < len(bingoBoards); index++ {
+		bingo := &bingoBoards[index]
+		bingo.Play()
+		if bingo.HasBingo() {
+			score := bingo.Score()
+			if bingo.NumTurns < quickestWinner.NumTurns {
+				fmt.Printf("Board %d won in %d moves with score %d.\n", index, bingo.NumTurns, score)
+				quickestWinner = bingo
+				continue
+			}
+		}
+	}
+
+	fmt.Printf("Quickest board won in %d moves with score %d.\n", quickestWinner.NumTurns, quickestWinner.Score())
 }
 
-func Day06() {
-	fishesAsString := util.ReadStringsFromFile("./inputs/day06-lanternfish.txt")[0]
-	fishies := util.StringToIntSlice(fishesAsString)
-
-	fmt.Println(submarine.SimulateLanternFishLife(fishies, 256))
+func Day03() {
+	bytes := util.ReadStringsFromFile("./inputs/day03.txt")
+	d := submarine.NewDiagnosticReport(bytes)
+	d.Process()
+	fmt.Println(d.LifeSupportRating())
 }
 
-func Day07() {
-	inputString := util.ReadStringsFromFile("./inputs/day07-crabs-in-submarines.txt")[0]
-	crabSubmarines := util.StringToIntSlice(inputString)
-
-	fmt.Println(submarine.FindMostEfficientFuelUsage(crabSubmarines))
-
+func Day02() {
+	instructions := util.ReadStringsFromFile("./inputs/day02.txt")
+	s := submarine.New()
+	s.ProcessInstructions(instructions)
+	fmt.Println(s.Result())
 }
+
+func Day01() {
+	depths := util.ReadIntegersFromFile("./inputs/day01.txt")
+	fmt.Println(countNumberOfPositiveChangesInDepth(depths))
+}
+
 func countNumberOfPositiveChangesInDepth(inputs []int) (result int) {
 	for index := 1; index < len(inputs); index++ {
 		if index+2 > len(inputs)-1 {
--- submarine/crab_mover.go
+++ submarine/crab_mover.go
@@ -1,7 +1,6 @@
 package submarine
 
 import (
-	"fmt"
 	"math"
 )
 
@@ -9,27 +8,15 @@ func CalculateCrabMove(crabs []int, pos int) int {
 	var totalFuelUsed int
 	for _, crab := range crabs {
 		steps := int(math.Abs(float64(crab - pos)))
-		for i := 0; i < steps; i++ {
-			totalFuelUsed += i
-		}
-		totalFuelUsed += steps
+		totalFuelUsed += steps + steps*(steps-1)/2
 	}
 	return totalFuelUsed
 }
 
-func FindMostEfficientFuelUsage(crabs []int) int {
-	lowestFuelUsage := 0
-
-	for i := 0; i < len(crabs); i++ {
-		fuelUsed := CalculateCrabMove(crabs, i)
-		if lowestFuelUsage == 0 {
-			lowestFuelUsage = fuelUsed
-		}
-		if fuelUsed < lowestFuelUsage {
-			lowestFuelUsage = fuelUsed
-		}
-		fmt.Println(i, fuelUsed)
+func FindMostEfficientFuelUsage(crabsInSubs []int) int {
+	lowestFuelUsage := CalculateCrabMove(crabsInSubs, 0)
+	for i := 0; i < len(crabsInSubs); i++ {
+		lowestFuelUsage = int(math.Min(float64(lowestFuelUsage), float64(CalculateCrabMove(crabsInSubs, i))))
 	}
-
 	return lowestFuelUsage
 }
